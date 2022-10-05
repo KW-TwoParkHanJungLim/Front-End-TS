d0381f8dd594a25704868a9b0104ca3ec4295994
Jiwoo Jeong <90768869+jiwoo0629@users.noreply.github.com>
2022-10-06 00:10:29 +0900

    Update Router.tsx

--- air-quality-dashboard/src/Router.tsx
+++ air-quality-dashboard/src/Router.tsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import LoginPage from './pages/jiwoo/LoginPage';
 import MainPage from './pages/jiwoo/MainPage';
+import Main_Sensor from './pages/jiwoo/Main_Sensor';
+import Main_Data from './pages/jiwoo/Main_Data';
 import TimeGraph from './pages/jiwoo/TimeGraph';
 import UserInfo from './pages/jiwoo/UserInfo';
 import ExportPage from './pages/jiwoo/ExportPage';
@@ -11,6 +13,8 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<LoginPage />}></Route>
         <Route path='/user/main' element={<MainPage />}></Route>
+        <Route path='/user/main/sensor' element={<Main_Sensor />}></Route>
+        <Route path='/user/main/sensor/data' element={<Main_Data />}></Route>
         <Route path='/user/graph' element={<TimeGraph />}></Route>
         <Route path='/user/info' element={<UserInfo />}></Route>
         <Route path='/user/export' element={<ExportPage />}></Route>
