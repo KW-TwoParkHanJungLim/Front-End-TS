23ce0fc3d6043f43adc93de15461d37fe8d256ae
Jiwoo Jeong <90768869+jiwoo0629@users.noreply.github.com>
2022-10-06 00:12:24 +0900

    Update SelectData.tsx

--- air-quality-dashboard/src/components/jiwoo/SelectData.tsx
+++ air-quality-dashboard/src/components/jiwoo/SelectData.tsx
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 
-export default class Calendar extends Component {
+export default class SelectData extends Component {
   render() {
     return(
       <select name="공기질" className="select">
         <option selected> 전체 </option>
-        <option value="1">공기질1</option>
+        <option value="1" onSelect={function() {
+                
+        }}>공기질1</option>
       </select>
     );
   } 
