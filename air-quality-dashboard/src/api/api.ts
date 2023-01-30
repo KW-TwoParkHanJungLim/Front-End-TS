38f17d94dc20cb3c596e9cc97c11956f4a2b62b9
mmsw1597 <mmsw1597@naver.com>
2023-01-30 17:15:24 +0900

    Test: 깃허브 페이지 백엔드 연결

--- air-quality-dashboard/src/api/api.ts
+++ air-quality-dashboard/src/api/api.ts
@@ -1,9 +1,11 @@
 import React from "react";
 import { getCookie } from "../JWT/cookie";
 
+const URL = `http://backend.hanseojin.shop:8088`;
+
 //2020-05-07
 export function fetchSensorAvg(date: string, userId: string, sensorId: string) {
-  return fetch(`/details?userId=${userId}&date=${date}&id=${sensorId}`, {
+  return fetch(`${URL}/details?userId=${userId}&date=${date}&id=${sensorId}`, {
     method: "GET",
     headers: {
       "X-AUTH-TOKEN": getCookie("token"),
@@ -20,7 +22,7 @@ export function fetchGraph(
   sensorIds: string[],
   attr: string
 ) {
-  let url = `/graph?collection=${collection}&logTime=${logTime}`;
+  let url = `${URL}/graph?collection=${collection}&logTime=${logTime}`;
   for (let x of sensorIds) {
     url += `&sensors=${x}`;
   }
@@ -38,7 +40,7 @@ export function fetchGraph(
 }
 
 export function fetchGraphSensorList(userId: string) {
-  return fetch(`/graph/sensors?userId=${userId}`, {
+  return fetch(`${URL}/graph/sensors?userId=${userId}`, {
     method: "GET",
     headers: {
       "X-AUTH-TOKEN": getCookie("token"),
@@ -50,7 +52,7 @@ export function fetchGraphSensorList(userId: string) {
 }
 
 export function fetchMain(UserId: string) {
-  return fetch(`/main?userId=${UserId}`, {
+  return fetch(`${URL}/main?userId=${UserId}`, {
     method: "GET",
     headers: {
       "X-AUTH-TOKEN": getCookie("token"),
@@ -63,7 +65,7 @@ export function fetchMain(UserId: string) {
 
 //로그인한 사용자의 정보를 불러오는 함수
 export function fetchUser(UserId: any) {
-  return fetch(`/user/profile?id=${UserId}`, {
+  return fetch(`${URL}/user/profile?id=${UserId}`, {
     method: "GET",
     headers: {
       "X-AUTH-TOKEN": getCookie("token"),
@@ -76,7 +78,7 @@ export function fetchUser(UserId: any) {
 
 //관리자용 전체 사용자 리스트 불러오는 함수
 export function fetchUserList() {
-  return fetch(`/admin/userList`, {
+  return fetch(`${URL}/admin/userList`, {
     method: "GET",
     headers: {
       "X-AUTH-TOKEN": getCookie("token"),
