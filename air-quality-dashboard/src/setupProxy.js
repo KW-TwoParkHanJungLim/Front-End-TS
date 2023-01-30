f63d3d37d79411110ada831e2db37a1881bf4eb0
mmsw1597 <mmsw1597@naver.com>
2023-01-30 15:51:51 +0900

    Fix: 배포된 서버로 프록시 주소 변경

--- air-quality-dashboard/src/setupProxy.js
+++ air-quality-dashboard/src/setupProxy.js
@@ -2,8 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
+    "/login",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://backend.hanseojin.shop:8088",
       changeOrigin: true,
     })
   );
